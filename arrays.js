c544c9bd539fe8410f6520057907eb20430c211c
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-04-24 17:11:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray (chocolateBars, foo){
 }
 
 function addElementToEndOfArray (chocolateBars, foo){
-  return [chocolateBars, 1]
+  return [...chocolateBars, foo]
 }
 
 function destructivelyAddElementToEndOfArray (chocolateBars, foo){
