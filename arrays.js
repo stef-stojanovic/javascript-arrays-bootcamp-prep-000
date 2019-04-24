67f8dab584af41b59bfc56d470dd54f324477c97
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-04-24 16:28:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray (chocolateBars, 'foo'){
+  var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+  return ['foo', chocolateBars]
+}
