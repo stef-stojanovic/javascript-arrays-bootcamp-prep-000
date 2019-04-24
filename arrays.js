2183f3133538344144b2ae4a23927eee10fee182
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-04-24 17:31:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,8 +33,8 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  var array = [1, 2, 3]
-  array.pop()
-  return array
+  var array = [1, 2, 3];
+  array.pop();
+  return array;
 }
 
